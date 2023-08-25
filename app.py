64ab2c5709413bdcda0ee3ae0b7fbc3682d8e9d5
Marios <marios_2510@hotmail.com>
2023-08-25 12:04:33 +0300

    Add error handling.

--- app.py
+++ app.py
@@ -18,16 +18,34 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload_image():
+    message = None
     colors = None
-    if 'file' not in request.files:
-        return redirect(url_for('index'))
-    file = request.files['file']
-    if file.filename == '':
-        return redirect(url_for('index'))
-    if file and allowed_file(file.filename):
-        filepath = save_file(file, app.config['UPLOAD_FOLDER'])
-        colors = get_top_colors(filepath)
-    return render_template('index.html', colors=colors)
+    uploaded_image_path = None
+
+    try:
+        if 'file' not in request.files:
+            message = "No file part in the request."
+            return render_template('index.html', message=message)
+
+        file = request.files['file']
+
+        if file.filename == '':
+            message = "No file selected."
+            return render_template('index.html', message=message)
+
+        if file and allowed_file(file.filename):
+            filepath = save_file(file, app.config['UPLOAD_FOLDER'])
+            uploaded_image_path = filepath
+            colors = get_top_colors(filepath)
+        else:
+            message = "Invalid file type. Please upload an image."
+    
+    except Exception as e:
+        # This will catch any unexpected errors during processing.
+        message = "An error occurred while processing the image. Please try again."
+
+    return render_template('index.html', colors=colors, uploaded_image_path=uploaded_image_path, message=message)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
